91c35fdf6102792c42ecac903a59cf27743b287d
Nathan Crapo <nathan_crapo@yahoo.com>
2022-04-27 23:14:15 -0600

    Add googletest to the project

---
+++ subsquares/subsquares1_test.cpp
@@ -0,0 +1,216 @@
+#include "subsquares.h"
+#include <gtest/gtest.h>
+using namespace n4n;
+
+TEST(NoSquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 0, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, false);
+    EXPECT_EQ(result.size, 0);
+}
+
+TEST(EmtpySquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, false);
+    EXPECT_EQ(result.size, 0);
+}
+
+TEST(FullSquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 8);
+}
+
+TEST(EmbeddedSquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 1, 1, 1, 0, 0, 1 },
+        { 1, 0, 1, 0, 1, 0, 0, 1 },
+        { 1, 0, 1, 1, 1, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 8);
+}
+
+TEST(TopLeftSquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 1, 1, 1, 0, 0, 1, 1, 1 },
+        { 1, 0, 1, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 0, 0, 0, 1 },
+        { 1, 0, 1, 0, 1, 0, 0, 1 },
+        { 1, 0, 1, 1, 1, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 3);
+}
+
+TEST(TopRightSquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 0, 1, 0, 0, 1, 1, 1, 1 },
+        { 1, 0, 1, 0, 1, 0, 0, 1 },
+        { 1, 1, 1, 0, 1, 0, 0, 1 },
+        { 1, 0, 1, 0, 1, 1, 1, 1 },
+        { 1, 0, 1, 1, 1, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 0, 0, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 4);
+    EXPECT_EQ(result.size, 4);
+}
+
+TEST(BottomRightSquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 1, 1, 1, 1, 1, 1 },
+        { 0, 0, 1, 0, 0, 0, 0, 1 },
+        { 0, 0, 1, 0, 0, 0, 0, 1 },
+        { 0, 0, 1, 0, 0, 0, 0, 1 },
+        { 0, 0, 1, 0, 0, 0, 0, 1 },
+        { 0, 0, 1, 1, 1, 1, 1, 1 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 2);
+    EXPECT_EQ(result.column, 2);
+    EXPECT_EQ(result.size, 6);
+}
+
+TEST(BottomLeftSquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 1, 1, 1, 1, 1, 1 },
+        { 0, 0, 1, 0, 0, 0, 0, 1 },
+        { 0, 0, 1, 0, 0, 0, 0, 1 },
+        { 0, 0, 1, 0, 0, 0, 0, 1 },
+        { 1, 1, 1, 0, 0, 0, 0, 1 },
+        { 1, 1, 0, 1, 1, 1, 1, 1 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 6);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 2);
+}
+
+// Ensures that Optimization 5 doesn't cause square detection to fail
+TEST(OptimizedTailSquare, Basic)
+{
+    // clang-format off
+    int matrix[max_square_size][max_square_size] = {
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 1, 1, 1, 1, 0, 0 },
+        { 0, 0, 1, 0, 1, 0, 0, 0 },
+        { 0, 0, 1, 1, 1, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+    };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 2);
+    EXPECT_EQ(result.column, 2);
+    EXPECT_EQ(result.size, 3);
+}
---
+++ subsquares/subsquares2_test.cpp
@@ -0,0 +1,336 @@
+#include "subsquares.h"
+#include <gtest/gtest.h>
+using namespace n4n;
+
+TEST(NoSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        0, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, false);
+    EXPECT_EQ(result.size, 0);
+}
+
+TEST(EmtpySquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, false);
+    EXPECT_EQ(result.size, 0);
+}
+
+TEST(FullSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 8);
+}
+
+TEST(EmbeddedSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        1, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 1, 1, 0, 0, 1,
+        1, 0, 1, 0, 1, 0, 0, 1,
+        1, 0, 1, 1, 1, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 8);
+}
+
+TEST(TopLeftSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        1, 1, 1, 1, 0, 1, 1, 1,
+        1, 0, 1, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 0, 0, 0, 1,
+        1, 0, 1, 0, 1, 0, 0, 1,
+        1, 0, 1, 1, 1, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 3);
+}
+
+TEST(TopRightSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        0, 1, 1, 1, 1, 1, 1, 1,
+        1, 0, 1, 0, 1, 0, 0, 1,
+        1, 1, 1, 0, 1, 0, 0, 1,
+        1, 0, 1, 0, 1, 1, 1, 1,
+        1, 0, 1, 1, 1, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 4);
+    EXPECT_EQ(result.size, 4);
+}
+
+TEST(BottomRightSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 0, 0, 0, 1,
+        0, 0, 1, 0, 0, 0, 0, 1,
+        0, 0, 1, 0, 0, 0, 0, 1,
+        0, 0, 1, 0, 0, 0, 0, 1,
+        0, 0, 1, 1, 1, 1, 1, 1,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 2);
+    EXPECT_EQ(result.column, 2);
+    EXPECT_EQ(result.size, 6);
+}
+
+TEST(BottomLeftSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 0, 0, 0, 1,
+        0, 0, 1, 0, 0, 0, 0, 1,
+        0, 0, 1, 0, 0, 0, 0, 1,
+        1, 1, 1, 0, 0, 0, 0, 1,
+        1, 1, 0, 1, 1, 1, 1, 1,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 6);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 2);
+}
+
+// Ensures that Optimization 5 doesn't cause square detection to fail
+TEST(OptimizedTailSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 0, 0,
+        0, 0, 1, 0, 1, 0, 0, 0,
+        0, 0, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    };
+    matrix_t matrix = { matrix_data, 8 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 2);
+    EXPECT_EQ(result.column, 2);
+    EXPECT_EQ(result.size, 3);
+}
+
+TEST(SmallSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        1, 1, 1,
+        1, 0, 1,
+        1, 1, 1,
+    };
+    matrix_t matrix = { matrix_data, 3 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 3);
+}
+
+TEST(SmallSquare2, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        1, 1, 1, 0,
+        1, 0, 1, 0,
+        1, 1, 1, 0,
+	0, 0, 0, 0
+    };
+    matrix_t matrix = { matrix_data, 4 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 3);
+}
+
+TEST(SmallNoSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        1, 1, 1,
+        1, 0, 1,
+        1, 1, 0,
+    };
+    matrix_t matrix = { matrix_data, 3 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, false);
+    EXPECT_EQ(result.size, 0);
+}
+
+TEST(SmallestSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        1, 1,
+        1, 1,
+    };
+    matrix_t matrix = { matrix_data, 2 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 2);
+}
+
+TEST(BiggestSquare, FlexibleIface)
+{
+    // clang-format off
+    int matrix_data[] = {
+        1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    };
+    matrix_t matrix = { matrix_data, 16 };
+    // clang-format on
+
+    square result;
+    auto found = find_max_subsquare(matrix, result);
+
+    EXPECT_EQ(found, true);
+    EXPECT_EQ(result.row, 0);
+    EXPECT_EQ(result.column, 0);
+    EXPECT_EQ(result.size, 2);
+}
